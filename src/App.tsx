212316e3b0ceb426ce045df7fecdf5325130bcab
ThomasStudioRosell <thomas@studiorosell.com>
2024-03-25 08:50:35 +0100

    Removed fps meter

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,8 @@ import { Canvas } from "@react-three/fiber"
 import './styles/App.css';
 import { Scene } from './scenes/Scene';
 import { Physics } from '@react-three/cannon';
-import { initFpsStats } from './components/FpsStats';
 
 function App() {
-  initFpsStats()
   return (
     <div className="flex h-screen">
       <Canvas id='canvas'>
